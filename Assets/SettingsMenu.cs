d0f000d083d3ee6880a4a43eef0221f25aea642b
AnaisGs <anaisgoulamhoussensatsou@gmail.com>
2025-04-25 09:54:24 +0200

    audiomixer

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
  public void SetVolume(float volume)
  {
-    Debug.Log(volume);
+
+    audioMixer.SetFloat("volume", volume);
  }
 }
 
